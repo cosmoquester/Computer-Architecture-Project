7d9b4f2fdf5f158876c5562d4b675474bf5b4ce7
Cho Kyu Won <yyj10137@naver.com>
2018-05-16 18:23:13 +0900

    sqrt assembly
    sqrt assembly

---
+++ Phase 2/sqrt.asm
@@ -0,0 +1,11 @@
+.sqrt:
+addi $s0, $zero, 0x1     //s0=x=1, x=float
+add $s1, $zero, $zero    // s1=i=0
+.loop_sqrt:
+div.s $t0, $a0, $s0       // t0=input/x
+add.s $t0, $t0, $a0       // t0= x+input/x
+divi $s0, $t0, 0x2        // x=t0/2
+addi $s1, $s1, 0x1        // i++
+bne $s1, 10, loop_sqrt    // if i!=10, loop
+addi $v0, $zero, $s1      // v0=x
+ret                       //return
